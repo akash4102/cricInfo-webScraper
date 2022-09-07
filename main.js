c7ee0d492d073f0cdb5728af6b0acd8eab070a1d
Akash Verma <aakashverma4102@gmail.com>
2022-09-07 17:57:34 +0530

    extract url and find all links

---
+++ main.js
@@ -0,0 +1,53 @@
+const url="https://www.espncricinfo.com/series/ipl-2020-21-1210595";
+const tempurl="https://www.espncricinfo.com";
+
+//venue date opponedt result runs balls fours sixes sr
+const request =require("request");
+const cheerio=require("cheerio");
+const { futimesSync } = require("fs");
+request(url,cb);
+function cb(err,request,html){
+    if(err){
+        console.log(err);
+    }
+    else{
+        extractLink(html);
+    }
+}
+function extractLink(html){
+    let $=cheerio.load(html);;
+    let anchorElem=$(".ds-border-t.ds-border-line.ds-text-center.ds-py-2");
+    let tt=$(anchorElem[0]).html();
+    let templink1="";
+    for(let i=0;i<tt.length;i++){
+        if(tt[i]=='h' && tt[i+1]=='r' && tt[i+2]=='e' && tt[i+3]=='f' ){
+            let k=i+6;
+            while(tt[k]!='"'){
+                templink1+=tt[k];
+                k++;
+            }
+            break;
+        }
+    }
+    let link=tempurl+templink1;
+    getAllMatchesLink(link);
+}
+function getAllMatchesLink(url){
+    request(url,function(err,response,html){
+        if(err){
+            console.log(err);
+        }
+        else{
+            extractAllLinks(html);
+        }
+    })
+}
+function extractAllLinks(html){
+    let $=cheerio.load(html);
+    let anchorElem=$(".ds-grow.ds-px-4.ds-border-r.ds-border-line-default-translucent>a");
+    for(let i=0;i<anchorElem.length;i++){
+        let tt=$(anchorElem[i]).attr('href');
+        console.log(tempurl+tt);
+    }
+
+}
